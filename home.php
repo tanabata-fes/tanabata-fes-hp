47e5e6209a71fdf6645fb5fe4813e631a40dc3e7
Masashi Aso <masashi.apple.japan@icloud.com>
2022-03-07 03:27:56 +0900

    fix: template_urlを使ってパスを通した

--- home.php
+++ home.php
@@ -1,8 +1,10 @@
 <!-- head -->
 <?php if (wp_is_mobile()): ?>
 	<!-- header -->
-	<!-- メイン（mobile/home） -->
+	<?php get_template_part('mobile/home'); ?>
+	<!-- footer -->
 <?php else: ?>
 	<!-- header -->
 	<?php get_template_part('desktop/home'); ?>
+	<!-- footer -->
 <?php endif; ?>
