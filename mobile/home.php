9874582bc7930526a934d72fe3d3f0af6e07db4c
Masashi Aso <masashi.apple.japan@icloud.com>
2022-03-07 04:18:33 +0900

    style: 空行を削除

--- mobile/home.php
+++ mobile/home.php
@@ -5,14 +5,10 @@
 <body>
 <div id="home">
 	<div id="n_199">
-		<img id="n_146" src="<?php bloginfo('template_url'); ?>/mobile/images/home/n_146.png" srcset="<?php bloginfo('template_url'); ?>/mobile/images/home/n_146.png 1x, <?php bloginfo('template_url'); ?>/mobile/images/home/n_146@2x.png 2x">
-			
-		</svg>
+		<img id="n_146" src="<?php bloginfo('template_url'); ?>/mobile/images/home/n_146.png" srcset="<?php bloginfo('template_url'); ?>/mobile/images/home/n_146.png 1x, <?php bloginfo('template_url'); ?>/mobile/images/home/n_146@2x.png 2x">		</svg>
 	</div>
 	<div id="Logo">
-		<img id="n_26" src="<?php bloginfo('template_url'); ?>/mobile/images/home/n_26.png" srcset="<?php bloginfo('template_url'); ?>/mobile/images/home/n_26.png 1x, <?php bloginfo('template_url'); ?>/mobile/images/home/n_26@2x.png 2x">
-			
-		</svg>
+		<img id="n_26" src="<?php bloginfo('template_url'); ?>/mobile/images/home/n_26.png" srcset="<?php bloginfo('template_url'); ?>/mobile/images/home/n_26.png 1x, <?php bloginfo('template_url'); ?>/mobile/images/home/n_26@2x.png 2x">		</svg>
 	</div>
 	<svg class="orangeBackground">
 		<rect id="orangeBackground" rx="0" ry="0" x="0" y="0" width="390" height="325">
@@ -20,9 +16,7 @@
 	</svg>
 	<div id="n_245">
 		<div id="n_121">
-			<img id="n_120" src="<?php bloginfo('template_url'); ?>/mobile/images/home/n_120.png" srcset="<?php bloginfo('template_url'); ?>/mobile/images/home/n_120.png 1x, <?php bloginfo('template_url'); ?>/mobile/images/home/n_120@2x.png 2x">
-				
-			</svg>
+			<img id="n_120" src="<?php bloginfo('template_url'); ?>/mobile/images/home/n_120.png" srcset="<?php bloginfo('template_url'); ?>/mobile/images/home/n_120.png 1x, <?php bloginfo('template_url'); ?>/mobile/images/home/n_120@2x.png 2x">			</svg>
 		</div>
 		<div id="n_244">
 			<svg class="n_80">
@@ -43,9 +37,7 @@
 	<div id="n_250">
 		<div id="n_246">
 			<div id="n_123">
-				<img id="n_122" src="<?php bloginfo('template_url'); ?>/mobile/images/home/n_122.png" srcset="<?php bloginfo('template_url'); ?>/mobile/images/home/n_122.png 1x, <?php bloginfo('template_url'); ?>/mobile/images/home/n_122@2x.png 2x">
-					
-				</svg>
+				<img id="n_122" src="<?php bloginfo('template_url'); ?>/mobile/images/home/n_122.png" srcset="<?php bloginfo('template_url'); ?>/mobile/images/home/n_122.png 1x, <?php bloginfo('template_url'); ?>/mobile/images/home/n_122@2x.png 2x">				</svg>
 			</div>
 			<svg class="n_81">
 				<rect id="n_81" rx="20" ry="20" x="0" y="0" width="300" height="310">
@@ -120,18 +112,14 @@
 			<rect id="n_81_bw" rx="20" ry="20" x="0" y="0" width="300" height="151">
 			</rect>
 		</svg>
-		<img id="n_134" src="<?php bloginfo('template_url'); ?>/mobile/images/home/n_134.png" srcset="<?php bloginfo('template_url'); ?>/mobile/images/home/n_134.png 1x, <?php bloginfo('template_url'); ?>/mobile/images/home/n_134@2x.png 2x">
-			
-		</svg>
+		<img id="n_134" src="<?php bloginfo('template_url'); ?>/mobile/images/home/n_134.png" srcset="<?php bloginfo('template_url'); ?>/mobile/images/home/n_134.png 1x, <?php bloginfo('template_url'); ?>/mobile/images/home/n_134@2x.png 2x">		</svg>
 	</div>
 	<div id="Text_by">
 		<span>カウントダウン埋め込み</span>
 	</div>
 	<div id="n_248">
 		<div id="n_197">
-			<img id="n_136" src="<?php bloginfo('template_url'); ?>/mobile/images/home/n_136.png" srcset="<?php bloginfo('template_url'); ?>/mobile/images/home/n_136.png 1x, <?php bloginfo('template_url'); ?>/mobile/images/home/n_136@2x.png 2x">
-				
-			</svg>
+			<img id="n_136" src="<?php bloginfo('template_url'); ?>/mobile/images/home/n_136.png" srcset="<?php bloginfo('template_url'); ?>/mobile/images/home/n_136.png 1x, <?php bloginfo('template_url'); ?>/mobile/images/home/n_136@2x.png 2x">			</svg>
 		</div>
 		<div id="n_198">
 			<svg class="n_115">
@@ -251,62 +239,48 @@
 					</path>
 				</svg>
 				<div id="n_7">
-					<img id="n_6" src="<?php bloginfo('template_url'); ?>/mobile/images/home/n_6.png" srcset="<?php bloginfo('template_url'); ?>/mobile/images/home/n_6.png 1x, <?php bloginfo('template_url'); ?>/mobile/images/home/n_6@2x.png 2x">
-						
-					</svg>
+					<img id="n_6" src="<?php bloginfo('template_url'); ?>/mobile/images/home/n_6.png" srcset="<?php bloginfo('template_url'); ?>/mobile/images/home/n_6.png 1x, <?php bloginfo('template_url'); ?>/mobile/images/home/n_6@2x.png 2x">					</svg>
 				</div>
 				<svg class="n_9" viewBox="1 5.674 1.647 0.457">
 					<path id="n_9" d="M 1 6.13100004196167 L 2.647000074386597 5.673999786376953 L 1 6.13100004196167 Z">
 					</path>
 				</svg>
 				<div id="n_9_db">
-					<img id="n_8_dc" src="<?php bloginfo('template_url'); ?>/mobile/images/home/n_8_dc.png" srcset="<?php bloginfo('template_url'); ?>/mobile/images/home/n_8_dc.png 1x, <?php bloginfo('template_url'); ?>/mobile/images/home/n_8_dc@2x.png 2x">
-						
-					</svg>
+					<img id="n_8_dc" src="<?php bloginfo('template_url'); ?>/mobile/images/home/n_8_dc.png" srcset="<?php bloginfo('template_url'); ?>/mobile/images/home/n_8_dc.png 1x, <?php bloginfo('template_url'); ?>/mobile/images/home/n_8_dc@2x.png 2x">					</svg>
 				</div>
 				<svg class="n_10" viewBox="3.074 0.999 0.88 1.443">
 					<path id="n_10" d="M 3.953999996185303 2.441999912261963 L 3.073999881744385 0.9990000128746033 L 3.953999996185303 2.441999912261963 Z">
 					</path>
 				</svg>
 				<div id="n_11">
-					<img id="n_10_df" src="<?php bloginfo('template_url'); ?>/mobile/images/home/n_10_df.png" srcset="<?php bloginfo('template_url'); ?>/mobile/images/home/n_10_df.png 1x, <?php bloginfo('template_url'); ?>/mobile/images/home/n_10_df@2x.png 2x">
-						
-					</svg>
+					<img id="n_10_df" src="<?php bloginfo('template_url'); ?>/mobile/images/home/n_10_df.png" srcset="<?php bloginfo('template_url'); ?>/mobile/images/home/n_10_df.png 1x, <?php bloginfo('template_url'); ?>/mobile/images/home/n_10_df@2x.png 2x">					</svg>
 				</div>
 				<svg class="n_25" viewBox="0 0 1.101 1.349">
 					<path id="n_25" d="M 1.101000308990479 0 L 0 1.348999977111816">
 					</path>
 				</svg>
 				<div id="n_13">
-					<img id="n_12" src="<?php bloginfo('template_url'); ?>/mobile/images/home/n_12.png" srcset="<?php bloginfo('template_url'); ?>/mobile/images/home/n_12.png 1x, <?php bloginfo('template_url'); ?>/mobile/images/home/n_12@2x.png 2x">
-						
-					</svg>
+					<img id="n_12" src="<?php bloginfo('template_url'); ?>/mobile/images/home/n_12.png" srcset="<?php bloginfo('template_url'); ?>/mobile/images/home/n_12.png 1x, <?php bloginfo('template_url'); ?>/mobile/images/home/n_12@2x.png 2x">					</svg>
 				</div>
 				<svg class="n_11_dj" viewBox="8.11 5.928 1.562 0.61">
 					<path id="n_11_dj" d="M 9.671999931335449 6.538000106811523 L 8.109999656677246 5.927999973297119 L 9.671999931335449 6.538000106811523 Z">
 					</path>
 				</svg>
 				<div id="n_15">
-					<img id="n_14" src="<?php bloginfo('template_url'); ?>/mobile/images/home/n_14.png" srcset="<?php bloginfo('template_url'); ?>/mobile/images/home/n_14.png 1x, <?php bloginfo('template_url'); ?>/mobile/images/home/n_14@2x.png 2x">
-						
-					</svg>
+					<img id="n_14" src="<?php bloginfo('template_url'); ?>/mobile/images/home/n_14.png" srcset="<?php bloginfo('template_url'); ?>/mobile/images/home/n_14.png 1x, <?php bloginfo('template_url'); ?>/mobile/images/home/n_14@2x.png 2x">					</svg>
 				</div>
 			</div>
 			<div onclick="application.goToTargetView(event)" id="Text_dm">
 				<span>アクセス</span>
 			</div>
 			<div id="n_18">
-				<img id="n_17" src="<?php bloginfo('template_url'); ?>/mobile/images/home/n_17.png" srcset="<?php bloginfo('template_url'); ?>/mobile/images/home/n_17.png 1x, <?php bloginfo('template_url'); ?>/mobile/images/home/n_17@2x.png 2x">
-					
-				</svg>
+				<img id="n_17" src="<?php bloginfo('template_url'); ?>/mobile/images/home/n_17.png" srcset="<?php bloginfo('template_url'); ?>/mobile/images/home/n_17.png 1x, <?php bloginfo('template_url'); ?>/mobile/images/home/n_17@2x.png 2x">				</svg>
 			</div>
 			<div onclick="application.goToTargetView(event)" id="Text_dp">
 				<span>プライバシーポリシー</span>
 			</div>
 			<div id="n_20">
-				<img id="n_19" src="<?php bloginfo('template_url'); ?>/mobile/images/home/n_19.png" srcset="<?php bloginfo('template_url'); ?>/mobile/images/home/n_19.png 1x, <?php bloginfo('template_url'); ?>/mobile/images/home/n_19@2x.png 2x">
-					
-				</svg>
+				<img id="n_19" src="<?php bloginfo('template_url'); ?>/mobile/images/home/n_19.png" srcset="<?php bloginfo('template_url'); ?>/mobile/images/home/n_19.png 1x, <?php bloginfo('template_url'); ?>/mobile/images/home/n_19@2x.png 2x">				</svg>
 			</div>
 			<div onclick="application.goToTargetView(event)" id="Text_ds">
 				<span>お問い合わせ</span>
@@ -344,9 +318,7 @@
 						</div>
 					</div>
 					<div id="n_30">
-						<img id="n_29" src="<?php bloginfo('template_url'); ?>/mobile/images/home/n_29.png" srcset="<?php bloginfo('template_url'); ?>/mobile/images/home/n_29.png 1x, <?php bloginfo('template_url'); ?>/mobile/images/home/n_29@2x.png 2x">
-							
-						</svg>
+						<img id="n_29" src="<?php bloginfo('template_url'); ?>/mobile/images/home/n_29.png" srcset="<?php bloginfo('template_url'); ?>/mobile/images/home/n_29.png 1x, <?php bloginfo('template_url'); ?>/mobile/images/home/n_29@2x.png 2x">						</svg>
 					</div>
 				</div>
 				<div id="n_41">
@@ -390,9 +362,7 @@
 						</svg>
 					</div>
 					<div id="n_39">
-						<img id="n_33" src="<?php bloginfo('template_url'); ?>/mobile/images/home/n_33.png" srcset="<?php bloginfo('template_url'); ?>/mobile/images/home/n_33.png 1x, <?php bloginfo('template_url'); ?>/mobile/images/home/n_33@2x.png 2x">
-							
-						</svg>
+						<img id="n_33" src="<?php bloginfo('template_url'); ?>/mobile/images/home/n_33.png" srcset="<?php bloginfo('template_url'); ?>/mobile/images/home/n_33.png 1x, <?php bloginfo('template_url'); ?>/mobile/images/home/n_33@2x.png 2x">						</svg>
 						<div id="n_">
 							<span>本年度テーマは<br/>「満開」に決定！</span>
 						</div>
@@ -435,9 +405,7 @@
 						</div>
 					</div>
 					<div id="n_30_e">
-						<img id="n_29_e" src="<?php bloginfo('template_url'); ?>/mobile/images/home/n_29_e.png" srcset="<?php bloginfo('template_url'); ?>/mobile/images/home/n_29_e.png 1x, <?php bloginfo('template_url'); ?>/mobile/images/home/n_29_e@2x.png 2x">
-							
-						</svg>
+						<img id="n_29_e" src="<?php bloginfo('template_url'); ?>/mobile/images/home/n_29_e.png" srcset="<?php bloginfo('template_url'); ?>/mobile/images/home/n_29_e.png 1x, <?php bloginfo('template_url'); ?>/mobile/images/home/n_29_e@2x.png 2x">						</svg>
 					</div>
 				</div>
 				<div id="n_41_e">
@@ -481,9 +449,7 @@
 						</svg>
 					</div>
 					<div id="n_39_fk">
-						<img id="n_33_fl" src="<?php bloginfo('template_url'); ?>/mobile/images/home/n_33_fl.png" srcset="<?php bloginfo('template_url'); ?>/mobile/images/home/n_33_fl.png 1x, <?php bloginfo('template_url'); ?>/mobile/images/home/n_33_fl@2x.png 2x">
-							
-						</svg>
+						<img id="n_33_fl" src="<?php bloginfo('template_url'); ?>/mobile/images/home/n_33_fl.png" srcset="<?php bloginfo('template_url'); ?>/mobile/images/home/n_33_fl.png 1x, <?php bloginfo('template_url'); ?>/mobile/images/home/n_33_fl@2x.png 2x">						</svg>
 						<div id="n__fm">
 							<span>本年度テーマは<br/>「満開」に決定！</span>
 						</div>
@@ -506,9 +472,7 @@
 		</svg>
 		<div id="header_fr">
 			<div id="Logo_fs">
-				<img id="n_26_ft" src="<?php bloginfo('template_url'); ?>/mobile/images/home/n_26_ft.png" srcset="<?php bloginfo('template_url'); ?>/mobile/images/home/n_26_ft.png 1x, <?php bloginfo('template_url'); ?>/mobile/images/home/n_26_ft@2x.png 2x">
-					
-				</svg>
+				<img id="n_26_ft" src="<?php bloginfo('template_url'); ?>/mobile/images/home/n_26_ft.png" srcset="<?php bloginfo('template_url'); ?>/mobile/images/home/n_26_ft.png 1x, <?php bloginfo('template_url'); ?>/mobile/images/home/n_26_ft@2x.png 2x">				</svg>
 			</div>
 			<div id="n_233">
 				<div id="n_191">
