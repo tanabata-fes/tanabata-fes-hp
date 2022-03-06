f60f99caa8b46797f6e6048ff5d719ecf71c9395
Jumpei Funayama <contact.zouri.music@gmail.com>
2022-03-07 03:56:31 +0900

    feat:mobile版のaboutページ

---
+++ mobile/about.php
@@ -0,0 +1,226 @@
+<body>
+<div id="page-about">
+	<div id="n_217">
+		<div id="n_216">
+			<img id="n_215" src="<?php bloginfo('template_url'); ?>/mobile/images/about/n_215.png" srcset= "<?php bloginfo('template_url'); ?>/mobile/images/about/n_215.png 1x, <?php bloginfo('template_url'); ?>/mobile/images/about/n_215@2x.png 2x">
+				
+			</svg>
+		</div>
+		<div id="Text">
+			<span>七夕祭について</span>
+		</div>
+	</div>
+	<div id="text22pt595757" class="text22pt_595757">
+		<div id="Text_u">
+			<span>「満開」について</span>
+		</div>
+	</div>
+	<svg class="n_30">
+		<rect id="n_30" rx="20" ry="20" x="0" y="0" width="300" height="314">
+		</rect>
+	</svg>
+	<div id="text14pt595757" class="text14pt_595757">
+		<div id="n__SFC________">
+			<span>多くの機会が失われたこの数年、<br/>陽の光を浴びようと膨らんできた<br/>つぼみがSFCにはあります。<br/><br/>人と人とが出会い、手を取り合って<br/>楽しむ祭。<br/><br/>そんな非日常に溢れるキャンパスで、<br/>一人ひとりの笑顔が</span><span style="color:rgba(230,131,24,1);">「満開」</span><span>に<br/>咲き誇りますように<br/>という思いが込められています。</span>
+		</div>
+	</div>
+	<div id="text22pt595757_y" class="text22pt_595757">
+		<div id="Text_z">
+			<span>ロゴについて</span>
+		</div>
+	</div>
+	<div id="LOGO">
+		<img id="n_60" src= "<?php bloginfo('template_url'); ?>/mobile/images/about/n_60.png" srcset= "<?php bloginfo('template_url'); ?>/mobile/images/about/n_60.png 1x, <?php bloginfo('template_url'); ?>/mobile/images/about/n_60@2x.png 2x">
+			
+		</svg>
+	</div>
+	<div id="n_218">
+		<svg class="n_20" viewBox="0 0 280.702 355.093">
+			<path id="n_20" d="M 29.05815315246582 0 L 251.6436157226562 0 C 267.6919555664062 0 280.7017517089844 13.24839782714844 280.7017517089844 29.59112739562988 L 280.7017517089844 325.5023498535156 C 280.7017517089844 341.8451538085938 267.6919555664062 355.0934753417969 251.6436157226562 355.0934753417969 L 29.05815315246582 355.0934753417969 C 13.0097770690918 355.0934753417969 0 341.8451538085938 0 325.5023498535156 L 0 29.59112739562988 C 0 13.24839782714844 13.0097770690918 0 29.05815315246582 0 Z">
+			</path>
+		</svg>
+		<svg class="n_19" viewBox="0 0 280.702 347.852">
+			<path id="n_19" d="M 29.05815315246582 0 L 251.6436157226562 0 C 267.6919555664062 0 280.7017517089844 12.97822093963623 280.7017517089844 28.9876708984375 L 280.7017517089844 318.8643493652344 C 280.7017517089844 334.8738403320312 267.6919555664062 347.85205078125 251.6436157226562 347.85205078125 L 29.05815315246582 347.85205078125 C 13.0097770690918 347.85205078125 0 334.8738403320312 0 318.8643493652344 L 0 28.9876708984375 C 0 12.97822093963623 13.0097770690918 0 29.05815315246582 0 Z">
+			</path>
+		</svg>
+		<div id="Text_">
+			<span>朝顔</span>
+		</div>
+		<div id="n____">
+			<span>夏を象徴する花であると同時に、<br/>朝を告げる花でもあります。<br/>夜になるとしぼみ、自らが咲く時をじっと待つ花。花言葉は「愛情」「結束」「絆」。<br/>江戸時代には「牽牛花」（彦星を指す言葉）や「朝顔姫」（織姫と重ねてそう呼んだとされる）とも呼ばれており、朝顔が美しく咲くと<br/>『彦星と織姫の七夕の逢瀬が叶った』と言われていました。</span>
+		</div>
+	</div>
+	<svg class="n_20_" viewBox="0 0 280.702 211.381">
+		<path id="n_20_" d="M 29.05815315246582 0 L 251.6436157226562 0 C 267.6919555664062 0 280.7017517089844 7.886527538299561 280.7017517089844 17.61505317687988 L 280.7017517089844 193.7655639648438 C 280.7017517089844 203.4941253662109 267.6919555664062 211.380615234375 251.6436157226562 211.380615234375 L 29.05815315246582 211.380615234375 C 13.0097770690918 211.380615234375 0 203.4941253662109 0 193.7655639648438 L 0 17.61505317687988 C 0 7.886527538299561 13.0097770690918 0 29.05815315246582 0 Z">
+		</path>
+	</svg>
+	<svg class="n_19_" viewBox="0 0 280.702 204.97">
+		<path id="n_19_" d="M 29.05815315246582 0 L 251.6436157226562 0 C 267.6919555664062 0 280.7017517089844 7.647356986999512 280.7017517089844 17.08085250854492 L 280.7017517089844 187.8893585205078 C 280.7017517089844 197.3228759765625 267.6919555664062 204.97021484375 251.6436157226562 204.97021484375 L 29.05815315246582 204.97021484375 C 13.0097770690918 204.97021484375 0 197.3228759765625 0 187.8893585205078 L 0 17.08085250854492 C 0 7.647356986999512 13.0097770690918 0 29.05815315246582 0 Z">
+		</path>
+	</svg>
+	<div id="Text_ba">
+		<span>ツタ</span>
+	</div>
+	<div id="n_">
+		<span>糸のように緩やかに、されどしなやかに花どうしを結ぶツタ。<br/>集まった花たちの融和と、まっすぐではない祭りへの道のりを表しています。</span>
+	</div>
+	<svg class="n_78" viewBox="0 0 280.702 211.381">
+		<path id="n_78" d="M 29.05815315246582 0 L 251.6436157226562 0 C 267.6919555664062 0 280.7017517089844 7.886527538299561 280.7017517089844 17.61505317687988 L 280.7017517089844 193.7655639648438 C 280.7017517089844 203.4941253662109 267.6919555664062 211.380615234375 251.6436157226562 211.380615234375 L 29.05815315246582 211.380615234375 C 13.0097770690918 211.380615234375 0 203.4941253662109 0 193.7655639648438 L 0 17.61505317687988 C 0 7.886527538299561 13.0097770690918 0 29.05815315246582 0 Z">
+		</path>
+	</svg>
+	<svg class="n_79" viewBox="0 0 280.702 204.97">
+		<path id="n_79" d="M 29.05815315246582 0 L 251.6436157226562 0 C 267.6919555664062 0 280.7017517089844 7.647356986999512 280.7017517089844 17.08085250854492 L 280.7017517089844 187.8893585205078 C 280.7017517089844 197.3228759765625 267.6919555664062 204.97021484375 251.6436157226562 204.97021484375 L 29.05815315246582 204.97021484375 C 13.0097770690918 204.97021484375 0 197.3228759765625 0 187.8893585205078 L 0 17.08085250854492 C 0 7.647356986999512 13.0097770690918 0 29.05815315246582 0 Z">
+		</path>
+	</svg>
+	<div id="Text_bd">
+		<span>花火</span>
+	</div>
+	<div id="SFC">
+		<span>七夕祭の象徴であり、SFCの夏の風物詩。多くの花がしぼんでしまう夜こそ、明るく空を照らす夜の華です。天高くに向かって花が咲き誇る様を表しています。</span>
+	</div>
+	<div id="n_228">
+		<svg class="n_115">
+			<rect id="n_115" rx="20" ry="20" x="0" y="0" width="298.587" height="157">
+			</rect>
+		</svg>
+		<div id="n__bh">
+			<span>詳しい情報はこちらから<br/>広報資料をダウンロードしてご覧ください。</span>
+		</div>
+		<div id="n_138">
+			<svg class="n_38">
+				<rect id="n_38" rx="10" ry="10" x="0" y="0" width="148.882" height="42.773">
+				</rect>
+			</svg>
+			<div id="MB_">
+				<span>広報資料（~MB）<br/>ダウンロード</span>
+			</div>
+		</div>
+	</div>
+	<div id="n_24__2" class="________24___2">
+		<div id="Copyright__2023___All_Rights_R">
+			<span>Copyright ©️ 2023 慶應義塾大学 七夕祭 All Rights Reserved. </span>
+		</div>
+		<div id="n_201">
+			<div onclick="application.goToTargetView(event)" id="headerMenu" class="headerMenu">
+				<div id="Text_bp">
+					<span>ご協賛・ご寄付</span>
+				</div>
+			</div>
+			<div id="n_16">
+				<svg class="n_8" viewBox="5.082 7.828 0.136 1.724">
+					<path id="n_8" d="M 5.081999778747559 9.552000045776367 L 5.217999935150146 7.828000068664551 L 5.081999778747559 9.552000045776367 Z">
+					</path>
+				</svg>
+				<div id="n_7">
+					<img id="n_6" src= "<?php bloginfo('template_url'); ?>/mobile/images/about/n_6.png" srcset= "<?php bloginfo('template_url'); ?>/mobile/images/about/n_6.png 1x, <?php bloginfo('template_url'); ?>/mobile/images/about/n_6@2x.png 2x">
+						
+					</svg>
+				</div>
+				<svg class="n_9" viewBox="1 5.674 1.647 0.457">
+					<path id="n_9" d="M 1 6.13100004196167 L 2.647000074386597 5.673999786376953 L 1 6.13100004196167 Z">
+					</path>
+				</svg>
+				<div id="n_9_bv">
+					<img id="n_8_bw" src= "<?php bloginfo('template_url'); ?>/mobile/images/about/n_8_bw.png" srcset= "<?php bloginfo('template_url'); ?>/mobile/images/about/n_8_bw.png 1x, <?php bloginfo('template_url'); ?>/mobile/images/about/n_8_bw@2x.png 2x">
+						
+					</svg>
+				</div>
+				<svg class="n_10" viewBox="3.074 0.999 0.88 1.443">
+					<path id="n_10" d="M 3.953999996185303 2.441999912261963 L 3.073999881744385 0.9990000128746033 L 3.953999996185303 2.441999912261963 Z">
+					</path>
+				</svg>
+				<div id="n_11">
+					<img id="n_10_bz" src= "<?php bloginfo('template_url'); ?>/mobile/images/about/n_10_bz.png" srcset= "<?php bloginfo('template_url'); ?>/mobile/images/about/n_10_bz.png 1x, <?php bloginfo('template_url'); ?>/mobile/images/about/n_10_bz@2x.png 2x">
+						
+					</svg>
+				</div>
+				<svg class="n_25" viewBox="0 0 1.101 1.349">
+					<path id="n_25" d="M 1.101000308990479 0 L 0 1.348999977111816">
+					</path>
+				</svg>
+				<div id="n_13">
+					<img id="n_12" src= "<?php bloginfo('template_url'); ?>/mobile/images/about/n_12.png" srcset= "<?php bloginfo('template_url'); ?>/mobile/images/about/n_12.png 1x, <?php bloginfo('template_url'); ?>/mobile/images/about/n_12@2x.png 2x">
+						
+					</svg>
+				</div>
+				<svg class="n_11_b" viewBox="8.11 5.928 1.562 0.61">
+					<path id="n_11_b" d="M 9.671999931335449 6.538000106811523 L 8.109999656677246 5.927999973297119 L 9.671999931335449 6.538000106811523 Z">
+					</path>
+				</svg>
+				<div id="n_15">
+					<img id="n_14" src= "<?php bloginfo('template_url'); ?>/mobile/images/about/n_14.png" srcset= "<?php bloginfo('template_url'); ?>/mobile/images/about/n_14.png 1x, <?php bloginfo('template_url'); ?>/mobile/images/about/n_14@2x.png 2x">
+						
+					</svg>
+				</div>
+			</div>
+			<div onclick="application.goToTargetView(event)" id="Text_b">
+				<span>アクセス</span>
+			</div>
+			<div id="n_18">
+				<img id="n_17" src= "<?php bloginfo('template_url'); ?>/mobile/images/about/n_17.png" srcset= "<?php bloginfo('template_url'); ?>/mobile/images/about/n_17.png 1x, <?php bloginfo('template_url'); ?>/mobile/images/about/n_17@2x.png 2x">
+					
+				</svg>
+			</div>
+			<div onclick="application.goToTargetView(event)" id="Text_ca">
+				<span>プライバシーポリシー</span>
+			</div>
+			<div id="n_20_ca">
+				<img id="n_19_cb" src= "<?php bloginfo('template_url'); ?>/mobile/images/about/n_19_cb.png" srcset= "<?php bloginfo('template_url'); ?>/mobile/images/about/n_19_cb.png 1x, <?php bloginfo('template_url'); ?>/mobile/images/about/n_19_cb@2x.png 2x">
+					
+				</svg>
+			</div>
+			<div onclick="application.goToTargetView(event)" id="Text_cc">
+				<span>お問い合わせ</span>
+			</div>
+		</div>
+	</div>
+	<div id="header" class="header">
+		<svg class="n_119">
+			<rect id="n_119" rx="0" ry="0" x="0" y="0" width="390" height="110">
+			</rect>
+		</svg>
+		<div id="header_cf">
+			<div onclick="application.goToTargetView(event)" id="Logo">
+				<img onclick="application.goToTargetView(event)" id="n_26" src= "<?php bloginfo('template_url'); ?>/mobile/images/about/n_26.png" srcset= "<?php bloginfo('template_url'); ?>/mobile/images/about/n_26.png 1x, <?php bloginfo('template_url'); ?>/mobile/images/about/n_26@2x.png 2x">
+					
+				</svg>
+			</div>
+			<div onclick="application.goToTargetView(event)" id="n_233">
+				<div id="n_191">
+					<div onclick="application.goToTargetView(event)" id="Text_ck">
+						<span>慶應義塾大学</span>
+					</div>
+					<div onclick="application.goToTargetView(event)" id="Text_cl">
+						<span>第３３回七夕祭</span>
+					</div>
+				</div>
+			</div>
+			<div onclick="application.goToTargetView(event)" id="n_190">
+				<div id="n_189">
+					<svg class="n_1">
+						<ellipse id="n_1" rx="25" ry="25" cx="25" cy="25">
+						</ellipse>
+					</svg>
+					<div id="n_188">
+						<svg class="n_44" viewBox="0 0 21.5 2">
+							<path id="n_44" d="M 0 0 L 21.5 0">
+							</path>
+						</svg>
+						<svg class="n_45" viewBox="0 0 21.5 2">
+							<path id="n_45" d="M 0 0 L 21.5 0">
+							</path>
+						</svg>
+						<svg class="n_46" viewBox="0 0 21.5 2">
+							<path id="n_46" d="M 0 0 L 21.5 0">
+							</path>
+						</svg>
+					</div>
+				</div>
+				<div id="MENU">
+					<span>MENU</span>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</body>
