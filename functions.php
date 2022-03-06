506213f83586bd4d0011c36a598d7b539d9aacee
Jumpei Funayama <contact.zouri.music@gmail.com>
2022-03-07 03:58:21 +0900

    feat:is_mobile関数を追加

--- functions.php
+++ functions.php
@@ -1,10 +1,22 @@
 <?php
-function is_mobile() {
-  $size = $_SESSION[windowSize];
-  if ($size == 0) {
-    return (wp_is_mobile()) ? 'small' : 'wide'
-  } else {
-    return ($size < 768) ? 'small' : 'wide'
-  }
+function is_mobile(){
+$useragents = array(
+‘iPhone’, // iPhone
+‘iPod’, // iPod touch
+‘Android.*Mobile’, // 1.5+ Android *** Only mobile
+‘Windows.*Phone’, // *** Windows Phone
+‘dream’, // Pre 1.5 Android
+‘CUPCAKE’, // 1.5+ Android
+‘blackberry9500’, // Storm
+‘blackberry9530’, // Storm
+‘blackberry9520’, // Storm v2
+‘blackberry9550’, // Storm v2
+‘blackberry9800’, // Torch
+‘webOS’, // Palm Pre Experimental
+‘incognito’, // Other iPhone browser
+‘webmate’ // Other iPhone browser
+
+);
+$pattern = ‘/’.implode(‘|’, $useragents).’/i’;
+return preg_match($pattern, $_SERVER[‘HTTP_USER_AGENT’]);
 }
-?>
