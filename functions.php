f86364a116b1d13a5836176012787498a62c5df5
Jumpei Funayama <contact.zouri.music@gmail.com>
2022-03-07 04:14:42 +0900

    fix:多分"これ"なはず

--- functions.php
+++ functions.php
@@ -1,22 +1,22 @@
 <?php
 function is_mobile(){
 $useragents = array(
-‘iPhone’, // iPhone
-‘iPod’, // iPod touch
-‘Android.*Mobile’, // 1.5+ Android *** Only mobile
-‘Windows.*Phone’, // *** Windows Phone
-‘dream’, // Pre 1.5 Android
-‘CUPCAKE’, // 1.5+ Android
-‘blackberry9500’, // Storm
-‘blackberry9530’, // Storm
-‘blackberry9520’, // Storm v2
-‘blackberry9550’, // Storm v2
-‘blackberry9800’, // Torch
-‘webOS’, // Palm Pre Experimental
-‘incognito’, // Other iPhone browser
-‘webmate’ // Other iPhone browser
+"iPhone", // iPhone
+"iPod", // iPod touch
+"Android.*Mobile", // 1.5+ Android *** Only mobile
+"Windows.*Phone", // *** Windows Phone
+"dream", // Pre 1.5 Android
+"CUPCAKE", // 1.5+ Android
+"blackberry9500", // Storm
+"blackberry9530", // Storm
+"blackberry9520", // Storm v2
+"blackberry9550", // Storm v2
+"blackberry9800", // Torch
+"webOS", // Palm Pre Experimental
+"incognito", // Other iPhone browser
+"webmate" // Other iPhone browser
 
 );
-$pattern = ‘/’.implode(‘|’, $useragents).’/i’;
-return preg_match($pattern, $_SERVER[‘HTTP_USER_AGENT’]);
+$pattern = "/".implode("|", $useragents)."/i";
+return preg_match($pattern, $_SERVER["HTTP_USER_AGENT"]);
 }
