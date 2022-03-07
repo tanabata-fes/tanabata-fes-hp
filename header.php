a37c5e7bbd5abab451c1d15c3af4bd1fc81b7c74
Masashi Aso <masashi.apple.japan@icloud.com>
2022-03-07 16:25:13 +0900

    fix: 横幅に揃ってなかったのを修正

--- header.php
+++ header.php
@@ -14,73 +14,73 @@
     <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/img/favicon.ico"> <title>七夕祭</title>
 
     <?php if (wp_is_mobile()): ?>
-        <script src="<?php echo get_template_directory_uri(); ?>/mobile/script.js"></script>
+        <script id="applicationScript" type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/mobile/script.js"></script>
     <?php else: ?>
-        <script src="<?php echo get_template_directory_uri(); ?>/desktop/script.js"></script>
+        <script id="applicationScript" type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/desktop/script.js"></script>
     <?php endif; ?>
 
     <link rel="stylesheet" type="text/css" href="<?php bloginfo( 'stylesheet_url' ); ?>" media="all" />
     <?php if ( is_home() ): ?>
         <?php if (wp_is_mobile()): ?>
-            <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/mobile/css/home.css">
+            <link rel="stylesheet" type="text/css" id="applicationStylesheet" href="<?php echo get_template_directory_uri(); ?>/mobile/css/home.css">
         <?php else: ?>
-            <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/desktop/css/home.css">
+            <link rel="stylesheet" type="text/css" id="applicationStylesheet" href="<?php echo get_template_directory_uri(); ?>/desktop/css/home.css">
         <?php endif; ?>
     <?php endif; ?>
     <?php if ( is_page('about') ): ?>
         <?php if (wp_is_mobile()): ?>
-            <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/mobile/css/about.css">
+            <link rel="stylesheet" type="text/css" id="applicationStylesheet" href="<?php echo get_template_directory_uri(); ?>/mobile/css/about.css">
         <?php else: ?>
-            <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/desktop/css/about.css">
+            <link rel="stylesheet" type="text/css" id="applicationStylesheet" href="<?php echo get_template_directory_uri(); ?>/desktop/css/about.css">
         <?php endif; ?>
     <?php endif; ?>
     <?php if ( is_page('access') ): ?>
         <?php if (wp_is_mobile()): ?>
-            <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/mobile/css/access.css">
+            <link rel="stylesheet" type="text/css" id="applicationStylesheet" href="<?php echo get_template_directory_uri(); ?>/mobile/css/access.css">
         <?php else: ?>
-            <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/desktop/css/access.css">
+            <link rel="stylesheet" type="text/css" id="applicationStylesheet" href="<?php echo get_template_directory_uri(); ?>/desktop/css/access.css">
         <?php endif; ?>
     <?php endif; ?>
     <?php if ( is_page('committee') ): ?>
         <?php if (wp_is_mobile()): ?>
-            <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/mobile/css/committee.css">
+            <link rel="stylesheet" type="text/css" id="applicationStylesheet" href="<?php echo get_template_directory_uri(); ?>/mobile/css/committee.css">
         <?php else: ?>
-            <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/desktop/css/committee.css">
+            <link rel="stylesheet" type="text/css" id="applicationStylesheet" href="<?php echo get_template_directory_uri(); ?>/desktop/css/committee.css">
         <?php endif; ?>
     <?php endif; ?>
     <?php if ( is_page('contact') ): ?>
         <?php if (wp_is_mobile()): ?>
-            <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/mobile/css/home.css">
+            <link rel="stylesheet" type="text/css" id="applicationStylesheet" href="<?php echo get_template_directory_uri(); ?>/mobile/css/home.css">
         <?php else: ?>
-            <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/desktop/css/home.css">
+            <link rel="stylesheet" type="text/css" id="applicationStylesheet" href="<?php echo get_template_directory_uri(); ?>/desktop/css/home.css">
         <?php endif; ?>
     <?php endif; ?>
     <?php if ( is_page('donation') ): ?>
         <?php if (wp_is_mobile()): ?>
-            <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/mobile/css/donation.css">
+            <link rel="stylesheet" type="text/css" id="applicationStylesheet" href="<?php echo get_template_directory_uri(); ?>/mobile/css/donation.css">
         <?php else: ?>
-            <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/desktop/css/donation.css">
+            <link rel="stylesheet" type="text/css" id="applicationStylesheet" href="<?php echo get_template_directory_uri(); ?>/desktop/css/donation.css">
         <?php endif; ?>
     <?php endif; ?>
     <?php if ( is_page('group') ): ?>
         <?php if (wp_is_mobile()): ?>
-            <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/mobile/css/group.css">
+            <link rel="stylesheet" type="text/css" id="applicationStylesheet" href="<?php echo get_template_directory_uri(); ?>/mobile/css/group.css">
         <?php else: ?>
-            <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/desktop/css/group.css">
+            <link rel="stylesheet" type="text/css" id="applicationStylesheet" href="<?php echo get_template_directory_uri(); ?>/desktop/css/group.css">
         <?php endif; ?>
     <?php endif; ?>
     <?php if ( is_page('privacy') ): ?>
         <?php if (wp_is_mobile()): ?>
-            <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/mobile/css/privacy.css">
+            <link rel="stylesheet" type="text/css" id="applicationStylesheet" href="<?php echo get_template_directory_uri(); ?>/mobile/css/privacy.css">
         <?php else: ?>
-            <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/desktop/css/privacy.css">
+            <link rel="stylesheet" type="text/css" id="applicationStylesheet" href="<?php echo get_template_directory_uri(); ?>/desktop/css/privacy.css">
         <?php endif; ?>
     <?php endif; ?>    
     <?php if ( is_single() ): ?>
         <?php if (wp_is_mobile()): ?>
-            <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/mobile/css/single.css">
+            <link rel="stylesheet" type="text/css" id="applicationStylesheet" href="<?php echo get_template_directory_uri(); ?>/mobile/css/single.css">
         <?php else: ?>
-            <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/desktop/css/single.css">
+            <link rel="stylesheet" type="text/css" id="applicationStylesheet" href="<?php echo get_template_directory_uri(); ?>/desktop/css/single.css">
         <?php endif; ?>
     <?php endif; ?>
 </head>
