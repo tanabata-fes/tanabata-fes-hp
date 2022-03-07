bd8724d8cb26524a514287b883852be9939397d5
Jumpei Funayama <contact.zouri.music@gmail.com>
2022-03-08 07:15:28 +0900

    feat:フォントを有効化

--- functions.php
+++ functions.php
@@ -32,4 +32,39 @@ function get_recently_post() {
     echo "/";
   }
 }
+
+/*-----------------------------------
+  ヘッダー整理
+-----------------------------------*/
+// 絵文字を使うためのスクリプトとスタイルを出力しない
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
+ 
+// 絵文字の DNS プリフェッチ用リンクを出力しない（この場合はフィルターフックを利用）
+add_filter( 'emoji_svg_url', '__return_false' ); 
+ 
+// REST API URL を出力しない
+remove_action('wp_head','rest_output_link_wp_head');
+ 
+// EditURI を出力しない
+remove_action( 'wp_head', 'rsd_link' );
+ 
+// wlwmanifest を出力しない
+remove_action( 'wp_head', 'wlwmanifest_link' );
+ 
+// WordPress のバージョン情報の出力をしない（機能に影響なし）
+remove_action( 'wp_head', 'wp_generator' );
+ 
+// 短縮形 URL を出力をしない 	
+remove_action('wp_head', 'wp_shortlink_wp_head');
+ 
+// oEmbed 機能に必要なリンクを出力をしない
+remove_action('wp_head','rest_output_link_wp_head');
+remove_action('wp_head','wp_oembed_add_discovery_links');
+remove_action('wp_head','wp_oembed_add_host_js');
+ 
+// feed 出力をしない.
+remove_action( 'wp_head', 'feed_links', 2 );
+remove_action( 'wp_head', 'feed_links_extra', 3 );
+
 ?>
