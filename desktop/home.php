5c29fb6b9e7ab5998fbbcd12029ac76dbb6fc578
Jumpei Funayama <contact.zouri.music@gmail.com>
2022-03-08 15:57:58 +0900

    fix:残ってたリンク

--- desktop/home.php
+++ desktop/home.php
@@ -171,7 +171,7 @@
           </rect>
         </svg>
         <div id="MB_">
-          <a href="tanabata-fes.com/pr.pdf"><span>広報資料（7.3MB）<br/>ダウンロード</span></a>
+          <a href="https://tanabata-fes.com/pr.pdf"><span>広報資料（7.3MB）<br/>ダウンロード</span></a>
         </div>
       </div>
     </div>
@@ -187,7 +187,7 @@
           </rect>
         </svg>
         <div id="MB__fp">
-          <a href="tanabata-fes.com/biz.pdf"><span>渉外資料（2.7MB）<br/>ダウンロード</span></a>
+          <a href="https://tanabata-fes.com/biz.pdf"><span>渉外資料（2.7MB）<br/>ダウンロード</span></a>
         </div>
       </div>
     </div>
