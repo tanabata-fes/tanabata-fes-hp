434442e80e26cfce2155ad4a01312fbcbca65c50
Jumpei Funayama <t21649jf@sfc.keio.ac.jp>
2022-07-01 18:37:39 +0000

    もろもろいじったよ

--- desktop/donation.php
+++ desktop/donation.php
@@ -249,10 +249,10 @@
 			<img src="<?php bloginfo('template_url'); ?>/desktop/images/donation/kobayashi-kensetsu.png" style="widdth 485px"></a>
 		</div>
 		<div id="houmei">
-			<img src="1.png">
-			<img src="2.png">
-			<img src="3.png">
-			<img src="4.png">
+			<img src="<?php bloginfo('template_url'); ?>/desktop/images/donation/1.png">
+			<img src="<?php bloginfo('template_url'); ?>/desktop/images/donation/2.png">
+			<img src="<?php bloginfo('template_url'); ?>/desktop/images/donation/3.png">
+			<img src="<?php bloginfo('template_url'); ?>/desktop/images/donation/4.png">
 		</div>
 	</div>
 	<div id="Text_cx">
