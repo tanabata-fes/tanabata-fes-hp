dbab1ef71da6a77c49e59630c74f9a30ce44fa30
Jumpei Funayama <t21649jf@sfc.keio.ac.jp>
2022-07-05 03:02:08 +0000

    とりあえずデスクトップ

--- desktop/home.php
+++ desktop/home.php
@@ -348,6 +348,9 @@
     <div class="n_180">
     <a href="https://keiiku.gr.jp/"><img src="<?php bloginfo('template_url'); ?>/desktop/images/donation/keiiku.jpg" style="width:485px; position: absolute;top: 3725px; left: 360px;"></a>
     </div>
+    <div class="n_180">
+    <a href="https://www.big-power.co.jp/"><img src="<?php bloginfo('template_url'); ?>/desktop/images/desktop/bigpower.jpg" style="width:485px; position: absolute;top: 3925px; left: 360px;"></a>
+    </div>
     
     </div>
     <div id="n_197">
